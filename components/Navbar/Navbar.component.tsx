cbe3ff8e5bbbe37f1666fcb4e1bf78ac6be964e3
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-12 23:17:58 +0530

    Built: Meduim Nav

--- components/Navbar/Navbar.component.tsx
+++ components/Navbar/Navbar.component.tsx
@@ -3,12 +3,13 @@ import Link from 'next/link'
 
 // Icons
 import { FaBell } from 'react-icons/fa'
+import { IoSearch } from 'react-icons/io5'
 
 const Navbar = () => {
   return (
     <>
       <div className="flex h-10 w-full items-center justify-between">
-        <div className="flex items-center gap-4 ">
+        <div className="flex items-center gap-4 md:gap-10">
           <div className="h-7 w-auto">
             <img
               src="https://rb.gy/ulxxee"
@@ -17,11 +18,21 @@ const Navbar = () => {
               className="h-full w-full cursor-pointer object-contain"
             />
           </div>
-          <div>Browse</div>
+          <div className="md:hidden">Browse</div>
+          <div className='hidden md:flex gap-4'>
+            <span className='text-sm'>Home</span>
+            <span className='text-sm text-gray-400'>TV Shows</span>
+            <span className='text-sm text-gray-400'>Movies</span>
+            <span className='text-sm text-gray-400'>New & Popular</span>
+            <span className='text-sm text-gray-400'>My List</span>
+          </div>
         </div>
         <div className="flex items-center gap-5">
+          <div className='hidden md:block'>
+            <IoSearch className='h-6 w-6 text-white'/>
+          </div>
           <div>
-            <FaBell className='text-white w-5 h-5'/>
+            <FaBell className="h-5 w-5 text-white" />
           </div>
           <div>
             <Link href="/account">
