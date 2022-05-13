51360470d7d8f347b32906b2cb002f1044278147
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-14 00:19:58 +0530

    Updated: Scroller arrows to dynamic

--- components/Slider/Slider.component.tsx
+++ components/Slider/Slider.component.tsx
@@ -1,4 +1,5 @@
 import { Movie } from '../../typings'
+import { useRef, useState } from 'react'
 
 // Icons
 import { IoChevronBackSharp, IoChevronForwardOutline } from 'react-icons/io5'
@@ -10,6 +11,19 @@ interface Props {
 }
 
 const Slider = ({ title, movies }: Props) => {
+    const rowRef=useRef<HTMLDivElement>(null)
+    const [isMoved,setIsMoved]=useState(false)
+
+    const handleClick=(direction:string)=>{
+        setIsMoved(true)
+        if(rowRef.current){
+            const {scrollLeft, clientWidth}=rowRef.current
+            const scrollTo=
+            direction==='left'?scrollLeft-clientWidth:scrollLeft+clientWidth
+            rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+        }
+    }
+
   return (
     <>
       <div>
@@ -17,13 +31,13 @@ const Slider = ({ title, movies }: Props) => {
           <h1>{title}</h1>
         </div>
         <div className="group flex items-center relative">
-          <IoChevronBackSharp className="md:h-8 md:w-8 h-6 w-6 cursor-pointer lg:opacity-0 transition duration-75 group-hover:opacity-100 absolute left-3 z-10" />
-          <div className='flex overflow-x-scroll gap-1 py-2 md:py-4 lg:py-4 md:gap-2 scrollbar-hide items-center cursor-pointer'>
+          <IoChevronBackSharp className={`md:h-8 md:w-8 h-6 w-6 cursor-pointer lg:opacity-0 transition duration-75 group-hover:opacity-100 absolute left-3 z-10 ${!isMoved&&"hidden"}`} onClick={()=>handleClick("left")}/>
+          <div className='flex overflow-x-scroll gap-1 py-2 md:py-4 lg:py-4 md:gap-2 scrollbar-hide items-center cursor-pointer' ref={rowRef}>
             {movies.map((movie) => (
               <Template movies={movie} key={movie.id} />
             ))}
           </div>
-          <IoChevronForwardOutline className="md:h-8 md:w-8 h-6 w-6 cursor-pointer lg:opacity-0 transition duration-75 group-hover:opacity-100 absolute right-3" />
+          <IoChevronForwardOutline className={`md:h-8 md:w-8 h-6 w-6 cursor-pointer lg:opacity-0 transition duration-75 group-hover:opacity-100 absolute right-3`} onClick={()=>handleClick("right")}/>
         </div>
       </div>
     </>
