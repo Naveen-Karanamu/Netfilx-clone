7d17b3bbef5a9138de2b461f21a6a0d8dcb8ed53
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-14 15:01:31 +0530

    Built: Login page UI

---
+++ pages/login.tsx
@@ -0,0 +1,66 @@
+// Compoennts
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Netflix Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <div className="relative h-screen w-screen ">
+          <Image
+            src="https://rb.gy/p2hphi"
+            layout="fill"
+            className="-z-10 opacity-60"
+            objectFit="cover"
+          />
+        </div>
+        <div className="shadow-lg">
+          <img
+            src="https://rb.gy/ulxxee"
+            className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className='flex items-center justify-center'>
+          <div className="absolute top-0 mx-4 mt-40 rounded bg-black bg-opacity-60 p-4 md:p-8">
+            <h1 className="text-4xl font-semibold">Sign In</h1>
+            <form className="py-6 md:py-7">
+              <div className="">
+                <label>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="mb-2 md:mb-3 w-full rounded bg-[#333333] px-4 py-3 md:py-4 text-gray-300 placeholder-[gray] outline-none focus:bg-[#454545]"
+                  />
+                </label>
+                <label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="w-full rounded-md bg-[#333333] px-4 py-3 md:py-4 text-gray-300 placeholder-[gray] outline-none focus:bg-[#454545]"
+                  />
+                </label>
+                <div className="flex justify-center">
+                  <button className="mt-6 md:mt-7 w-2/3 rounded-md bg-[#E50914] py-2 font-semibold">
+                    Sign In
+                  </button>
+                </div>
+                <div className="mt-3 md:mt-4 text-center">
+                  <span className="text-[gray]">New to Netflix? </span>
+                  <button className="hover:underline">Sign Up Now</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
